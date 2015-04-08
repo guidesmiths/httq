01331e542cad885f0c494162e60a2ddfc5d49675
cressie176 <scresswell@acuminous.co.uk>
2015-04-08 09:55:39 +0100

    Adding request to body warez

---
+++ tests/warez/requestToPayload.tests.js
@@ -0,0 +1,48 @@
+var format = require('util').format
+var assert = require('assert')
+var _ = require('lodash')
+var async = require('async')
+var express = require('express')
+var request = require('request')
+var bodyParser = require('body-parser')
+var requestToPayload = require('../..').warez.requestToPayload
+
+describe('requestToPayload', function() {
+
+    var server
+    var middleware
+
+    before(function(done) {
+        var app = express()
+        app.use(bodyParser.json())
+        server = app.post('/', function(req, res, next) {
+            middleware(req, res, function(err) {
+                err ? res.status(500).end() : res.status(204).end()
+            })
+        }).listen(3000, done)
+    })
+
+    after(function(done) {
+        server ? server.close(done) : done()
+    })
+
+    it('should construct a payload from the request', function(done) {
+
+        var ctx = {}
+        requestToPayload({}, ctx, function(err, _middleware) {
+            assert.ifError(err)
+            middleware = _middleware
+            request({method: 'POST', url: 'http://localhost:3000?foo=1', headers: { 'bar': 2 }, json: { baz: 3 } }, function(err, response, content) {
+                assert.ifError(err)
+                assert.equal(response.statusCode, 204)
+                assert.ok(ctx.payload.url)
+                assert.equal(ctx.payload.url.query.foo, 1)
+                assert.ok(ctx.payload.headers)
+                assert.equal(ctx.payload.headers.bar, 2)
+                assert.ok(ctx.payload.body)
+                assert.equal(ctx.payload.body.baz, 3)
+                done()
+            })
+        })
+    })
+})
